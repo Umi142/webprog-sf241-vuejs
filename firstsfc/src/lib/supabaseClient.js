f8de101f76209983129c5d730d062bddc6a9b1da
Yumilka Gutierrez <ypgutierrez@student.apc.edu.ph>
2026-01-23 15:33:11 +0800

    Update Supabase client url link
    Replaced environment variables with hardcoded Supabase URL and key.

--- firstsfc/src/lib/supabaseClient.js
+++ firstsfc/src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabasePublishableKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
+const supabaseUrl = 'https://volqbrgoiuipqumftlwn.supabase.co'
+const supabasePublishableKey = 'sb_publishable_rH50pJAvd2Lpqf0qqk5gPA_Bwa8zgIO'
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
